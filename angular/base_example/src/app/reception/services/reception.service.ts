1801d0350b6c0adb3ac5dd158602baaf11c7d934
Andrea Briganti <kbytesys@gmail.com>
2019-03-24 04:20:26 +0100

    base angular app

--- angular/base_example/src/app/reception/services/reception.service.ts
+++ angular/base_example/src/app/reception/services/reception.service.ts
@@ -4,6 +4,8 @@ import {Room} from '../models/room.model';
 import {Observable} from 'rxjs';
 import {environment} from '../../../environments/environment';
 import {map} from 'rxjs/operators';
+import {CheckinLog} from '../models/checkinlog.model';
+import {Checkin} from '../models/checkin.model';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +21,32 @@ export class ReceptionService {
       map((responseData) => convertApiRooms(responseData))
     );
   }
+
+  public getCheckinlogs(roomid?: number): Observable<Array<CheckinLog>> {
+
+    let requestOptions = {};
+
+    if (roomid) {
+      requestOptions = { params: { room: roomid } };
+    }
+
+    return this.http.get<Array<CheckinLog>>(`${environment.apiUrl}/checkinlog/`, requestOptions).pipe(
+      map((responseData) => convertApiCheckinlogs(responseData))
+    );
+  }
+
+  public getCheckins(roomid?: number): Observable<Array<Checkin>> {
+
+    let requestOptions = {};
+
+    if (roomid) {
+      requestOptions = { params: { room: roomid } };
+    }
+
+    return this.http.get<Array<Checkin>>(`${environment.apiUrl}/checkin/`, requestOptions).pipe(
+      map((responseData) => convertApiCheckins(responseData))
+    );
+  }
 }
 
 function convertApiRoom(data: any): Room {
@@ -30,4 +58,32 @@ function convertApiRoom(data: any): Room {
 
 function convertApiRooms(data: any): Array<Room> {
   return data.map((item) => convertApiRoom(item));
+}
+
+function convertApiCheckinlog(data: any): CheckinLog {
+  return {
+    date: new Date(Date.parse(data.date)),
+    message: data.message
+  };
+}
+
+function convertApiCheckinlogs(data: any): Array<CheckinLog> {
+  return data.map((item) => convertApiCheckinlog(item));
+}
+
+function convertApiCheckin(data: any): Checkin {
+  return {
+    id: data.id,
+    person: {
+      name: data.person.name
+    },
+    room: {
+      id: data.room.id,
+      name: data.room.name
+    }
+  }
+}
+
+function convertApiCheckins(data: any): Array<Checkin> {
+  return data.map((item) => convertApiCheckin(item));
 }
